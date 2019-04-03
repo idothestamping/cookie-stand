9dde64c357f3ad3a2959a3de6920f19f18f761b8
idothestamping <douglas.klemp@gmail.com>
2019-04-02 23:43:35 -0700

    Table created with contents and styling done

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-// This version is to use a single object of store names and data:
 'use strict';
 // materialize side menu on mobile
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
-
-var storeList = {
-  "1st and Pike": ['23', '65', '6.5'],
-  "SeaTac Airport": ['23', '65', '6.5'],
-  "Seattle Center": ['23', '65', '6.5'],
-  "Capitol Hill": ['23', '65', '6.5'],
-  "Alki": ['23', '65', '6.5']
-};
 
 var hours = [
   '6am',
@@ -30,42 +21,102 @@ var hours = [
   '8pm',
 ];
 
-document.body.onload = addStoreArry;
-var storeNames = document.getElementById('storeNames');
+// Lab-07 requirement: Table using contructors
+var allStores = [];
 
-function addStoreArry() {  // auto generate store names from list into sepcific div and auto add class
-  for(var i=0; i < Object.keys(storeList).length; i++){
-    var dayTotal = [];
-    var newDiv = document.createElement("div");
-    newDiv.className = 'storeNames';
-    var newContent = document.createTextNode(Object.keys(storeList)[i]);
-    newDiv.appendChild(newContent);  
-    var currentDiv = document.getElementById("storeNames");
-    var parent  = document.getElementById('test');
-    parent.appendChild(newDiv, parent);
+function Store(name, min, max, avg) {
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  this.customers = [];
+  this.cookiesPerHour = [];
+  this.cookiesPerDay = 0;
+  allStores.push(this);
+}
+// Store instances
+new Store('1st and Pike','23', '65', '6.5');
+new Store('SeaTac Airport','3', '24', '1.2');
+new Store('Seattle Center','11', '38', '3.7');
+new Store('Capitol Hill','20', '38', '2.3');
+new Store('Alki', '2', '16', '4.6');
 
-      for(var j=0; j < hours.length; j++){ // nested loop to auto generate each hour per store for set hours
-        var liEl = document.createElement('li'); 
-        var getRandomInt = function(){  // get min and max cutomer and avg cookie sale per customer
-          let min = Math.ceil(Object.values(storeList)[i][0]);
-          let max = Math.floor(Object.values(storeList)[i][1]);
-          return Math.floor((Math.random() * (max - min + 1)) + min * Object.values(storeList)[i][2]);
-        };
-        liEl.className = 'perHourData';
-        var cookiePerHourNumber = getRandomInt(); // get random customer intake within min max threshold
-        var cookiePerHourText = hours[j] + ': ' + getRandomInt() + ' cookies';
-        liEl.textContent = cookiePerHourText;
-        newDiv.appendChild(liEl); // add total sold per hour per store
-        dayTotal.push(cookiePerHourNumber); 
-        
-      };
-      // add total cookies sold per day per store
-      var dayTotalText = dayTotal.reduce(function(pv, cv) { return pv + cv; }, 0);
-      console.log(dayTotal);
-      newDiv.className = 'storeNames';
-      var totalContent = document.createTextNode('Total Cookies: ' + dayTotalText); 
-      newDiv.appendChild(totalContent);  
-      parent.appendChild(newDiv, parent);
+console.table(allStores);
+
+Store.prototype.randomCustomers = function(){
+  for(var i = 0; i < hours.length; i++){ 
+    // Save for reference:
+    // let min = Math.ceil(Object.values(allStores[0])[1]);
+    // let max = Math.floor(Object.values(allStores[0])[2]);
+    // let avg = Object.values(allStores[0])[3]);
+    this.customers.push(Math.floor((Math.random() * (this.max - this.min + 1)) + this.min));
   };
 };
 
+Store.prototype.cookiesBought = function(){
+  for(var i = 0; i < hours.length; i++){
+    this.cookiesPerHour.push(Math.ceil(this.customers[i] * this.avg));
+  }
+};
+
+Store.prototype.totalCookies = function(){
+  var total = 0;
+  for(var i = 0; i < hours.length; i++){
+    this.cookiesPerDay = this.cookiesPerHour.reduce(function(pv, cv) { return pv + cv; }, 0);
+  }
+};
+
+Store.prototype.render = function(){
+
+  this.randomCustomers();
+  this.cookiesBought();
+  this.totalCookies();
+
+  var storeTable = document.getElementById('myDynamicTable');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+    for (var i = 0; i < hours.length; i++){
+      tdEl = document.createElement('td');
+      tdEl.textContent = this.cookiesPerHour[i];
+      trEl.appendChild(tdEl);
+    };
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.cookiesPerDay;
+  trEl.appendChild(tdEl).classList.add('total');;
+  
+
+  storeTable.appendChild(trEl);
+};
+
+function makeHeaderRow() {
+  // Make a blank column 1 header cell
+  var storeTable = document.getElementById('myDynamicTable');
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = '';
+  trEl.appendChild(thEl);
+ 
+  for (var i = 0; i < hours.length; i++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = hours[i]; 
+    trEl.appendChild(thEl);
+    // Header text for Daily Total column.
+    if (i === hours.length - 1){ 
+      thEl = document.createElement('th');
+      thEl.textContent = 'Daily Location Total';
+      trEl.appendChild(thEl);
+      storeTable.appendChild(trEl);
+    };
+  };
+};
+
+function renderallStores() {
+  for (var i = 0; i < allStores.length; i++) {
+    allStores[i].render();
+  };
+};
+
+makeHeaderRow();
+renderallStores();
